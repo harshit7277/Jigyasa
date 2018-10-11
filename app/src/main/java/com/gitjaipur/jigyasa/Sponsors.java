4cb738feb48434a8fe31dd1f3e4f7a4bd4714b1e
Akshat Jain <jainakshat425@gmail.com>
2018-10-11 16:19:38 +0530

    Third Commit Akshat

--- app/src/main/java/com/gitjaipur/jigyasa/Sponsors.java
+++ app/src/main/java/com/gitjaipur/jigyasa/Sponsors.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,7 +39,25 @@ public class Sponsors extends Fragment {
 
         List<ItemObject> allItems = new ArrayList<ItemObject>();
 
-        allItems.add(new ItemObject( R.drawable.rsz_adhoc,"Adhoc Networks"));
+        allItems.add(new ItemObject( R.drawable.triumph,""));
+        allItems.add(new ItemObject( R.drawable.yb,""));
+        allItems.add(new ItemObject( R.drawable.tripsaa,""));
+        allItems.add(new ItemObject( R.drawable.zoomcar,""));
+        allItems.add(new ItemObject( R.drawable.rsz_short,""));
+        allItems.add(new ItemObject( R.drawable.tn,""));
+        allItems.add(new ItemObject( R.drawable.rsz_1corexart,""));
+        allItems.add(new ItemObject( R.drawable.rsz_arena_anim,""));
+        allItems.add(new ItemObject( R.drawable.rsz_forsk,""));
+        allItems.add(new ItemObject( R.drawable.rsz_hadda,""));
+        allItems.add(new ItemObject( R.drawable.rsz_idi_diar,""));
+        allItems.add(new ItemObject( R.drawable.rsz_kalam,""));
+        allItems.add(new ItemObject( R.drawable.rsz_kaya,""));
+        allItems.add(new ItemObject( R.drawable.rsz_kloh,""));
+        allItems.add(new ItemObject( R.drawable.rsz_onnbike,""));
+        allItems.add(new ItemObject( R.drawable.rsz_pitb,""));
+        allItems.add(new ItemObject( R.drawable.rsz_adhoc,""));
+
+
 
 
         return allItems;
